06013f41df9ac1f487ab96898ba76747fcf23f33
MA Yijun <ma.yijun.0@gmail.com>
2014-06-20 10:07:54 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZApi::Application.config.secret_key_base = 'e86ff752544ad3b3f8c7799601ff6e3b71fb82ff1f69021c96d0a886488ff829beaee787b67ca207f9678cb761f528fe980e1cd7bbc0589eec4a2cbc99b03af0'
